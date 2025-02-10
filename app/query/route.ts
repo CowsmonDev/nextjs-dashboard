fe4fe367b713e1a964ed4e3b3d3ac1d31612d9b2
Agustin Tomas Crespo <agustin.crespo22@gmail.com>
2025-02-09 21:35:36 -0300

    Chapter 6: Setting Up Your Database

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,36 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
+import process from "node:process";
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+// const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+const sql = postgres(process.env.POSTGRES_URL!,
+    {
+        db: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        port: Number(process.env.POSTGRES_PORT) ?? 5432,
+        //ssl: 'require'
+    });
 
-// 	return data;
-// }
+async function listInvoices() {
+    const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+    return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+    /*return Response.json({
+        message:
+            'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    });*/
+    try {
+        return Response.json(await listInvoices());
+    } catch (error) {
+        return Response.json({error}, {status: 500});
+    }
 }
